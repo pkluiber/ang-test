0b6f52ee594f52c52de0c8e9b7d0350b40516477
pkluiber <pkluiber@gmail.com>
2021-10-12 15:36:42 +0200

    step2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
