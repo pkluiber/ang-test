f722014092c4aa1fb60ca0175624b0f7bed2fe3b
Marco Jost <marco.jost@live.de>
2021-10-12 16:06:19 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { NavbarComponent } from './navbar/navbar.component';
-=======
 import { HeroComponent } from './hero/hero.component';
 import { FooterComponent } from './footer/footer.component';
->>>>>>> 0b6f52ee594f52c52de0c8e9b7d0350b40516477
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    NavbarComponent
-=======
+    NavbarComponent,
     HeroComponent,
     FooterComponent
->>>>>>> 0b6f52ee594f52c52de0c8e9b7d0350b40516477
   ],
   imports: [
     BrowserModule,
